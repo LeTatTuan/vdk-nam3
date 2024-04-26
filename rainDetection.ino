84a4837ada4f1e234afee49b0aa1c1d2e100c554
LeTatTuan <letattuan04052003@gmail.com>
2024-04-27 02:36:10 +0700

    add interrupt with button manual control and complete remote control

--- rainDetection.ino
+++ rainDetection.ino
@@ -12,11 +12,13 @@ Servo myServo;
 BlynkTimer timer;
 int timerID1, timerID2;
 
-int rainValue;
+int rainValue, pos;
 int mucCanhBao = 750;
-boolean runMode = 1;                     // 1/0: bật/tắt chế độ cảnh báo
-boolean isAlert = 0;                     // 1/0: hoạt động, ko hoạt động
-boolean isOpenCeilingCover = 1;          // 1/0: trạng thái mở/đóng màn che
+boolean buttonRunModeState = HIGH;
+boolean buttonControlCeilingControlState = HIGH;
+boolean runMode = 1;             // 1/0: bật/tắt chế độ cảnh báo
+boolean isAlert = 0;             // 1/0: hoạt động, ko hoạt động
+boolean isOpenCeilingCover = 1;  // 1/0: trạng thái mở/đóng màn che
 WidgetLED led(V0);
 
 #define BUTTON_WARNING D1                // button tắt bật chế độ cảnh bảo
@@ -32,6 +34,8 @@ WidgetLED led(V0);
 #define RUNMODE V3
 #define SERVO V4
 
+char messageBuffer[100];  // Buffer for formatted message
+
 void setup() {
   Serial.begin(115200);
   delay(100);
@@ -41,46 +45,46 @@ void setup() {
   pinMode(LED_MODE, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(POWER_PIN_RAIN_SENSOR, OUTPUT);
-  digitalWrite(BUZZER, LOW);  //Tắt buzzer
-  digitalWrite(LED_MODE, HIGH);
+  digitalWrite(BUZZER, LOW);  // Tắt buzzer
+  digitalWrite(LED_MODE, LOW);
 
   myServo.attach(SERVO_PIN);
 
-  attachInterrupt(digitalPinToInterrupt(BUTTON_WARNING), buttonWarningISR, FALLING);
-  attachInterrupt(digitalPinToInterrupt(BUTTON_CONTROL_CEILING_COVER), buttonCeilingCoverISR, FALLING);
-
-  BlynkEdgent.begin();  //bắt đầu quá trình kết nối và cấu hình Blynk Edgent, cho phép thiết bị kết nối và làm việc với Blynk IoT Platform.
+  BlynkEdgent.begin();  // bắt đầu quá trình kết nối và cấu hình Blynk Edgent, cho phép thiết bị kết nối và làm việc với Blynk IoT Platform.
   timerID1 = timer.setInterval(1000L, handleTimerID1);
-  //Sau mỗi 1s thì hàm handleTimerID1 sẽ được gọi
+  // Sau mỗi 1s thì hàm handleTimerID1 sẽ được gọi
 }
 
-void buttonWarningISR() {
-  runMode = !runMode;
-
-  digitalWrite(LED_MODE, runMode);
-  Serial.println("Run mode: " + String(runMode));
-  Blynk.virtualWrite(RUNMODE, runMode);
-
-  if (runMode) {
-    Serial.println("Chế độ cảnh báo đang bật.");
-  } else {
-    Serial.println("Chế độ cảnh báo đang tắt.");
-  }
-}
-
-void buttonCeilingCoverISR() {
-  isOpenCeilingCover = !isOpenCeilingCover;
-  controlCeilingCover();
-  Blynk.virtualWrite(SERVO, isOpenCeilingCover);
-  if (isOpenCeilingCover) {
-    Serial.println("Màn chắn đang mở, không có mưa.");
-  } else {
-    Serial.println("Màn chắn đang đóng, có mưa.");
-  }
-}
 void loop() {
-  BlynkEdgent.run();  //để xử lý các sự kiện liên quan đến kết nối và giao tiếp với Blynk IoT Platform,
-  timer.run();        //để quản lý và thực hiện các hành động được đặt lịch trình bằng Blynk Timer.
+  BlynkEdgent.run();  // để xử lý các sự kiện liên quan đến kết nối và giao tiếp với Blynk IoT Platform,
+  timer.run();        // để quản lý và thực hiện các hành động được đặt lịch trình bằng Blynk Timer.
+
+  if (digitalRead(BUTTON_WARNING) == LOW) {
+    if (buttonRunModeState == HIGH) {
+      buttonRunModeState = LOW;
+      runMode = !runMode;
+      digitalWrite(LED_MODE, runMode);
+
+      if (runMode) {
+        Serial.println("Chế độ cảnh báo đang bật.");
+      } else {
+        Serial.println("Chế độ cảnh báo đang tắt.");
+      }
+      Blynk.virtualWrite(RUNMODE, runMode);
+    }
+  } else {
+    buttonRunModeState = HIGH;
+  }
+
+  if (digitalRead(BUTTON_CONTROL_CEILING_COVER) == LOW) {
+    if (buttonControlCeilingControlState == HIGH) {
+      buttonControlCeilingControlState = LOW;
+      controlCeilingCover();
+      Blynk.virtualWrite(SERVO, isOpenCeilingCover);
+    }
+  } else {
+    buttonControlCeilingControlState = HIGH;
+  }
 }
 
 void handleTimerID1() {
@@ -109,11 +113,12 @@ void onAlert() {
   if (rainValue < mucCanhBao) {
     if (isAlert == 0) {
       isAlert = 1;
-      Blynk.logEvent("canhbao", String("Cảnh báo! Lượng mưa = " + String(rainValue) + " vượt quá mức cho phép!"));
+      sprintf(messageBuffer, "Cảnh báo! Lượng mưa = %d vượt quá mức cho phép!", rainValue);
+      Blynk.logEvent("canhbao", messageBuffer);
       digitalWrite(BUZZER, HIGH);
       Serial.println("Đã bật cảnh báo!");
 
-      //Thiết lập một timer với ID là timerID2 để kiểm tra lại lượng mưa sau một khoảng thời gian 10 giây (10,000 ms) và gọi hàm handleTimerID2() để xử lý.
+      // Thiết lập một timer với ID là timerID2 để kiểm tra lại lượng mưa sau một khoảng thời gian 10 giây (10,000 ms) và gọi hàm handleTimerID2() để xử lý.
       timerID2 = timer.setInterval(10000L, handleTimerID2);
     }
 
@@ -137,15 +142,15 @@ void offAlert() {
 }
 void handleTimerID2() {
   if (rainValue > mucCanhBao) {  // ko mưa
-    if (isAlert == 1) {          // nếu cảnh báo đang hoạt động thì
+    if (isAlert) {               // nếu cảnh báo đang hoạt động thì tắt cảnh báo
       isAlert = 0;
       digitalWrite(BUZZER, LOW);
       Serial.println("Đã tắt loa cảnh báo!");
     }
 
-    if (isOpenCeilingCover == 0) {  // nếu màn che đang đóng thì mở màn che ra
+    if (!isOpenCeilingCover) {  // nếu màn che đang đóng thì mở màn che ra
       isOpenCeilingCover = 1;
-      for (int pos = 180; pos >= 0; pos -= 5) {
+      for (pos = 180; pos >= 0; pos -= 5) {
         myServo.write(pos);
         delay(0);
       }
@@ -154,39 +159,40 @@ void handleTimerID2() {
   }
 }
 
-//Được sử dụng để đồng bộ hóa trạng thái của các nút ảo trên ứng dụng Blynk với trạng thái hiện tại của thiết bị.
+// Được sử dụng để đồng bộ hóa trạng thái của các nút ảo trên ứng dụng Blynk với trạng thái hiện tại của thiết bị.
 BLYNK_CONNECTED() {
   Blynk.syncVirtual(RUNMODE, MUCCANHBAO, SERVO);
 }
 
-//hàm này được sử dụng để cập nhật giá trị của biến mucCanhbao và in ra giá trị mới đó.
+// hàm này được sử dụng để cập nhật giá trị của biến mucCanhbao và in ra giá trị mới đó.
 BLYNK_WRITE(MUCCANHBAO) {
   mucCanhBao = param.asInt();
-  Serial.println("Mức cảnh báo hiện tại: " + String(mucCanhBao));
+  Serial.println("Mức cảnh báo hiện tại: ");
+  Serial.println(mucCanhBao);
 }
 
 BLYNK_WRITE(RUNMODE) {
   runMode = param.asInt();
   Serial.print("runMode: ");
   Serial.println(runMode);
-  digitalWrite(LED_MODE, LOW);
+  digitalWrite(LED_MODE, runMode);
 }
 
-//cập nhật giá trị của biến isOpenCeilingCover dựa trên giá trị của nút ảo SERVO từ ứng dụng Blynk
+// cập nhật giá trị của biến isOpenCeilingCover dựa trên giá trị của nút ảo SERVO từ ứng dụng Blynk
 BLYNK_WRITE(SERVO) {
   isOpenCeilingCover = !param.asInt();
   controlCeilingCover();
 }
 
 void controlCeilingCover() {
-  if (isOpenCeilingCover == 1) {  // kiểm tra trạng thái của màn che trước khi đóng màn che
-    for (int pos = 0; pos <= 180; pos += 5) {
+  if (isOpenCeilingCover) {  // kiểm tra trạng thái của màn che trước khi đóng màn che
+    for (pos = 0; pos <= 180; pos += 5) {
       myServo.write(pos);
       delay(0);
     }
     isOpenCeilingCover = 0;
   } else {  // kiểm tra trạng thái trước khi mở màn che
-    for (int pos = 180; pos >= 0; pos -= 5) {
+    for (pos = 180; pos >= 0; pos -= 5) {
       myServo.write(pos);
       delay(0);
     }
